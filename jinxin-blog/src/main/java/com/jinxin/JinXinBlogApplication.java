783755b1335e65af4470bbe728345d8d87720a0a
UPolaris <fanjun1997@outlook.com>
2023-04-19 13:00:12 +0800

    今心：添加启动类，修改yml文件

---
+++ jinxin-blog/src/main/java/com/jinxin/JinXinBlogApplication.java
@@ -0,0 +1,19 @@
+package com.jinxin;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 86188
+ * @version 1.0
+ * @description 今心Blog
+ * @data 2023/4/18 20:49
+ */
+@SpringBootApplication
+public class JinXinBlogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JinXinBlogApplication.class, args);
+    }
+
+}
